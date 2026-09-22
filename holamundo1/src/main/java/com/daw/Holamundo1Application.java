b9245c1ceac642e14758abb0ef845ea619276fa7
josegon7 <jgoncre1412@g.educaand.es>
2026-09-22 13:40:37 +0200

    proyect

---
+++ holamundo1/src/main/java/com/daw/Holamundo1Application.java
@@ -0,0 +1,13 @@
+package com.daw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Holamundo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Holamundo1Application.class, args);
+	}
+
+}
